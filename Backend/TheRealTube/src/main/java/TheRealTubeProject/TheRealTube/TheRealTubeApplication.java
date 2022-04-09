2548379e2125d4975608ff8c9a7879fa068b530c
Sebastian Szelest <seba3444gaming@gmail.com>
2022-04-10 00:24:43 +0200

    Templatki projektu/poczatek

---
+++ Backend/TheRealTube/src/main/java/TheRealTubeProject/TheRealTube/TheRealTubeApplication.java
@@ -0,0 +1,13 @@
+package TheRealTubeProject.TheRealTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRealTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRealTubeApplication.class, args);
+	}
+
+}
---
+++ Backend/TheRealTube/src/test/java/TheRealTubeProject/TheRealTube/TheRealTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package TheRealTubeProject.TheRealTube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRealTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
