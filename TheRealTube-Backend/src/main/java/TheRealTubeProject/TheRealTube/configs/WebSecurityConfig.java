355da2d69e8f2fe5dac525705a367e30ef054cee
Sebastian Szelest <seba3444gaming@gmail.com>
2022-05-08 18:07:42 +0200

    Zwracanie obiektu user z wszystkimi właściwościami

--- TheRealTube-Backend/src/main/java/TheRealTubeProject/TheRealTube/configs/WebSecurityConfig.java
+++ TheRealTube-Backend/src/main/java/TheRealTubeProject/TheRealTube/configs/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package TheRealTubeProject.TheRealTube.configs;
 
+import TheRealTubeProject.TheRealTube.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
